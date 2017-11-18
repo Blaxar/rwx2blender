8133a720835ae0f9cc7ece6cdd893b938b94f632
Blaxar Waldarax <blaxar.waldarax@gmail.com>
2017-11-18 18:00:34 +0100

    Turned script into a proper Blender module

--- rwx2blender.py
+++ rwx2blender.py
@@ -1,9 +1,21 @@
+import sys
+import os
 import re
 import fileinput
 import numpy as np
 from copy import copy, deepcopy
-import os
-import numpy as np
+from math import radians
+import bpy
+from bpy.props import *
+import bmesh
+
+bl_info = {"name": "rwx2blender",
+           "author": "Julien Bardagi (Blaxar Waldarax)",
+           "description": "Module to import Active Worlds RenderWare files (.rwx)",
+           "version": (0, 1, 0),
+           "blender": (2, 78, 0),
+           "location": "File > Export...",
+           "category": "Import-Export"}
 
 class RwxState:
 
@@ -311,8 +323,9 @@ class RwxParser:
                 
                 res = self._polygon_regex.match(line)
                 if res:
+                    v_len = int(self._integer_regex.findall(res.group(2))[0])
                     v_id = [ int(x) for x in self._integer_regex.findall(res.group(3)) ]
-                    self._current_scope.shapes.append(RwxPolygon(v_id,\
+                    self._current_scope.shapes.append(RwxPolygon(v_id[0:v_len],\
                                                                  state=self._current_scope.state))
                     continue
                 
@@ -332,3 +345,186 @@ class RwxParser:
 
     def __call__(self):
         return self._rwx_clump_stack[0]
+
+
+def add_attr_recursive(clump, name):
+
+    attr = []
+    attr.extend(getattr(clump, name))
+    
+    for c in clump.clumps:
+        attr.extend(add_attr_recursive(c, name))
+
+    return attr
+
+
+def add_vertices_recursive(clump, transform = np.identity(4)):
+
+    vertices = []
+    transform = transform * clump.state.transform
+    for v in clump.verts:
+        vert = transform * np.matrix([v[0], v[1], v[2], 1]).reshape((4,1))
+        vertices.append((vert[0], vert[1], vert[2]))
+
+    for c in clump.clumps:
+        vertices.extend(add_vertices_recursive(c))
+
+    return vertices
+
+
+def add_faces_recursive(clump, offset=0):
+
+    faces = []
+    tmp_faces = clump.faces
+    for tmp_face in tmp_faces:
+        faces.append((tmp_face[0]+offset, tmp_face[1]+offset, tmp_face[2]+offset))
+    
+    offset += len(clump.verts)
+    
+    for c in clump.clumps:
+        tmp_faces, offset = add_faces_recursive(c, offset)
+        faces.extend(tmp_faces)
+
+    return faces, offset
+
+
+def make_materials_recursive(ob, clump, folder, extension = "jpg"):
+
+    for shape in clump.shapes:
+        # Get material
+
+        if shape.state.texture:
+            mat = bpy.data.materials.get(shape.state.texture)
+            if mat is None:
+                # create material
+                mat = bpy.data.materials.new(name=shape.state.texture)
+                tex = bpy.data.textures.new(shape.state.texture, type = 'IMAGE')
+                tex.image = bpy.data.images.load(os.path.join(folder, "%s.%s" % (shape.state.texture, extension)))
+                mtex = mat.texture_slots.add()
+                mtex.texture = tex
+                mtex.texture_coords = 'UV'
+                mtex.use_map_color_diffuse = True 
+                mtex.use_map_color_emission = True 
+                mtex.emission_color_factor = 1.0
+                mtex.use_map_density = True 
+                mtex.mapping = 'FLAT'
+
+            unassigned = True
+            for material in ob.data.materials:
+                if material.name == shape.state.texture:
+                    unassigned = False
+                
+            if unassigned: ob.data.materials.append(mat)
+        
+    for sub_clump in clump.clumps:
+        make_materials_recursive(ob, sub_clump, folder, extension)
+        
+class Rwx2BlenderOperator(bpy.types.Operator):
+
+    bl_idname = "object.rwx2blender"
+    bl_label = "Import"
+    bl_options = {'REGISTER'}
+
+    filepath= StringProperty(
+        name="File Path",
+        description="Filepath used for importing RenderWare .rwx file",
+        maxlen=1024,
+        default="",
+        subtype='FILE_PATH')
+    texturepath= StringProperty(
+        name="Texture Path",
+        description="Path to the texture directory",
+        maxlen=1024,
+        default="",
+        subtype='DIR_PATH')
+
+    def invoke(self, context, event):
+        wm = bpy.context.window_manager
+        wm.fileselect_add(self)
+
+        return {'RUNNING_MODAL'}
+
+    def execute(self, context):
+        
+        filepath = self.filepath
+        texturepath = self.texturepath
+
+        if texturepath is None or texturepath == "":
+            texturepath = os.path.join(os.path.split(os.path.dirname(filepath))[0], "textures")
+            if os.path.isdir(texturepath):
+                self.report({'WARNING'}, "No texture directory specified, assuming %s" % texturepath)
+            else:
+                self.report({'ERROR'}, "No texture directory specified and could not guess any.")
+                return {'CANCELLED'}
+
+        try:
+            parser = RwxParser(filepath)
+            rwx_object = parser()
+        except Exception as exc:
+            self.report({'ERROR'}, "Could not parse input file (likely not a proper .rwx).")
+            return {'CANCELLED'}
+
+        if len(rwx_object.clumps) == 0:
+            self.report({'ERROR'}, "No clump registered after parsing the input file (likely not a proper .rwx).")
+            return {'CANCELLED'}
+
+        verts = add_vertices_recursive(rwx_object.clumps[0])
+        edges = []
+        faces, offset = add_faces_recursive(rwx_object.clumps[0])
+        faces_state = add_attr_recursive(rwx_object.clumps[0], "faces_state")
+        faces_uv = add_attr_recursive(rwx_object.clumps[0], "faces_uv")
+
+        mesh = bpy.data.meshes.new('Mesh')
+        ob = bpy.data.objects.new('Object', mesh)
+        
+        make_materials_recursive(ob, rwx_object.clumps[0], texturepath)
+
+        # Create mesh from given verts, edges, faces. Either edges or
+        # faces should be [], or you ask for problems
+        mesh.from_pydata(verts, edges, faces)
+        bm = bmesh.new()
+        bm.from_mesh(mesh)
+        uv_layer = bm.loops.layers.uv.verify()
+        bm.faces.layers.tex.verify()  # currently blender needs both layers.
+
+        # adjust UVs
+        for i, f in enumerate(bm.faces):
+            if faces_state[i].texture:
+                f.material_index = ob.data.materials.keys().index(faces_state[i].texture)
+                for j, l in enumerate(f.loops):
+                    l[uv_layer].uv = faces_uv[i][j]
+
+
+        bm.to_mesh(mesh)
+        bm.free()
+
+        mesh.use_auto_smooth = True
+        mesh.auto_smooth_angle = 3.14/3.0 
+        mesh.calc_normals()
+
+        ob.location = (0,0,0)
+        ob.scale = (10,10,10)
+        ob.rotation_euler = (radians(90), 0, 0)
+        ob.show_name = True 
+        # Link object to scene
+        bpy.context.scene.objects.link(ob)
+
+        # Update mesh with new data
+        mesh.update(calc_edges=True)
+        
+        return {'FINISHED'}
+
+def import_menu_func_rwx(self, context):
+    op = self.layout.operator(Rwx2BlenderOperator.bl_idname, text="Active Worlds RenderWare file (.rwx)")
+
+def register():
+    bpy.utils.register_class(Rwx2BlenderOperator)
+    bpy.types.INFO_MT_file_import.append(import_menu_func_rwx)
+
+def unregister():
+    bpy.utils.unregister_class(Rwx2BlenderOperator)
+    bpy.types.INFO_MT_file_import.remove(import_menu_func_rwx)
+
+if __name__ == "__main__":
+    register()
+
