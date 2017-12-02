76d96c2f878afb021d7c4adbc1429111913f58ca
Blaxar Waldarax <blaxar.waldarax@gmail.com>
2017-12-02 16:49:51 +0100

    Added more RwxState material properties

--- rwx2blender.py
+++ rwx2blender.py
@@ -2,13 +2,24 @@ import sys
 import os
 import re
 import fileinput
-from copy import copy, deepcopy
+from copy import copy
 from math import radians
 import mathutils as mu
-import bpy
-from bpy.props import *
-import bmesh
+from hashlib import blake2b
+
+in_blender = None
+
+try:
+    import bpy
+    from bpy.props import *
+    import bmesh
+except ModuleNotFoundError as mnf:
+    in_blender = False
+else:
+    in_blender = True
+
 from traceback import print_exc
+from enum import Enum
 
 bl_info = {"name": "rwx2blender",
            "author": "Julien Bardagi (Blaxar Waldarax)",
@@ -18,19 +29,57 @@ bl_info = {"name": "rwx2blender",
            "location": "File > Import...",
            "category": "Import-Export"}
 
+class LightSampling(Enum):
+    FACET = 1
+    VERTEX = 2
+
+class GeometrySampling(Enum):
+    POINTCOULD = 1
+    WIREFRAME = 2
+    SOLID = 3
+
+class TextureMode(Enum):
+    LIT = 1
+    FORESHORTEN = 2
+    FILTER = 3
+
+class MaterialMode(Enum):
+    NONE = None
+    NULL = 1
+    DOUBLE = 2
+
 class RwxState:
 
     def __init__(self):
 
-        self.color = None
-        self.surface = None
-        self.opacity = None
-        self.lightsampling = None
-        self.geometrysampling = None
-        self.texturemodes = None
-        self.materialmodes = None
+        # Material related properties start here
+        self.color = (0.0, 0.0, 0.0) # Red, Green, Blue
+        self.surface = (0.0, 0.0, 0.0) # Ambience, Diffusion, Specularity
+        self.opacity = 1.0
+        self.lightsampling = LightSampling.FACET
+        self.geometrysampling = GeometrySampling.SOLID
+        self.texturemodes = [TextureMode.LIT,] # There's possibly more than one mode enabled at a time (hence why we use an array)
+        self.materialmode = MaterialMode.NONE # Neither NULL nor DOUBLE: we only render one side of the polygon
         self.texture = None
+        # End of material related properties
+        
         self.transform = mu.Matrix.Identity(4)
+    
+    @property
+    def mat_signature(self):
+
+        h = blake2b(digest_size=5)
+
+        sign = [("%.3f" % x) for x in self.color]
+        sign.extend([("%.3f" % x) for x in self.surface])
+        sign.append("%.3f" % self.opacity)
+        sign.append(self.lightsampling.name)
+        sign.append(self.geometrysampling.name)
+        sign.extend([x.name for x in sorted(self.texturemodes)])
+        sign.append(self.materialmode.name)
+
+        h.update("".join([str(x) for x in sign]).replace(".","").lower().encode("utf-8"))
+        return "_".join([str(self.texture), h.hexdigest()])
 
 
 class RwxVertex:
@@ -103,6 +152,7 @@ class RwxScope:
                 states.append(shape.state)
 
         return states
+
     
 class RwxClump(RwxScope):
 
@@ -414,11 +464,15 @@ def make_materials_recursive(ob, clump, folder, extension = "jpg"):
     for shape in clump.shapes:
         # Get material
 
-        if shape.state.texture:
-            mat = bpy.data.materials.get(shape.state.texture)
-            if mat is None:
-                # create material
-                mat = bpy.data.materials.new(name=shape.state.texture)
+        mat_sign = shape.state.mat_signature
+        
+        mat = bpy.data.materials.get(mat_sign)
+        
+        if mat is None:
+            # create material
+            mat = bpy.data.materials.new(name=mat_sign)
+            
+            if shape.state.texture:
                 tex = bpy.data.textures.new(shape.state.texture, type = 'IMAGE')
                 tex.image = bpy.data.images.load(os.path.join(folder, "%s.%s" % (shape.state.texture, extension)))
                 mtex = mat.texture_slots.add()
@@ -427,114 +481,122 @@ def make_materials_recursive(ob, clump, folder, extension = "jpg"):
                 mtex.use_map_color_diffuse = True 
                 mtex.use_map_color_emission = True 
                 mtex.emission_color_factor = 1.0
-                mtex.use_map_density = True 
+                mtex.use_map_density = True
                 mtex.mapping = 'FLAT'
 
-            unassigned = True
-            for material in ob.data.materials:
-                if material.name == shape.state.texture:
-                    unassigned = False
-                
-            if unassigned: ob.data.materials.append(mat)
+            mat.alpha = shape.state.opacity
+            mat.diffuse_color = shape.state.color
+
+            ob.data.materials.append(mat)
+
+        else:
+            if mat_sign not in ob.data.materials.keys():
+                ob.data.materials.append(mat)
         
     for sub_clump in clump.clumps:
         make_materials_recursive(ob, sub_clump, folder, extension)
+
+if in_blender:
         
-class Rwx2BlenderOperator(bpy.types.Operator):
+    class Rwx2BlenderOperator(bpy.types.Operator):
 
-    bl_idname = "object.rwx2blender"
-    bl_label = "Import"
-    bl_options = {'REGISTER'}
+        bl_idname = "object.rwx2blender"
+        bl_label = "Import"
+        bl_options = {'REGISTER'}
 
-    filepath= StringProperty(
-        name="File Path",
-        description="Filepath used for importing RenderWare .rwx file",
-        maxlen=1024,
-        default="",
-        subtype='FILE_PATH')
-    texturepath= StringProperty(
-        name="Texture Path",
-        description="Path to the texture directory",
-        maxlen=1024,
-        default="",
-        subtype='DIR_PATH')
+        filepath= StringProperty(
+            name="File Path",
+            description="Filepath used for importing RenderWare .rwx file",
+            maxlen=1024,
+            default="",
+            subtype='FILE_PATH')
+        texturepath= StringProperty(
+            name="Texture Path",
+            description="Path to the texture directory",
+            maxlen=1024,
+            default="",
+            subtype='DIR_PATH')
 
-    def invoke(self, context, event):
-        wm = bpy.context.window_manager
-        wm.fileselect_add(self)
+        def invoke(self, context, event):
+            wm = bpy.context.window_manager
+            wm.fileselect_add(self)
 
-        return {'RUNNING_MODAL'}
+            return {'RUNNING_MODAL'}
 
-    def execute(self, context):
+        def execute(self, context):
         
-        filepath = self.filepath
-        texturepath = self.texturepath
+            filepath = self.filepath
+            texturepath = self.texturepath
 
-        if texturepath is None or texturepath == "":
-            texturepath = os.path.join(os.path.split(os.path.dirname(filepath))[0], "textures")
-            if os.path.isdir(texturepath):
-                self.report({'WARNING'}, "No texture directory specified, assuming %s" % texturepath)
-            else:
-                self.report({'ERROR'}, "No texture directory specified and could not guess any.")
+            if texturepath is None or texturepath == "":
+                texturepath = os.path.join(os.path.split(os.path.dirname(filepath))[0], "textures")
+                if os.path.isdir(texturepath):
+                    self.report({'WARNING'}, "No texture directory specified, assuming %s" % texturepath)
+                else:
+                    self.report({'ERROR'}, "No texture directory specified and could not guess any.")
+                    return {'CANCELLED'}
+
+            try:
+                parser = RwxParser(filepath)
+                rwx_object = parser()
+            except Exception as exc:
+                print_exc(exc)
+                self.report({'ERROR'}, "Could not parse input file (either not a proper .rwx or a bug on my part).")
                 return {'CANCELLED'}
 
-        try:
-            parser = RwxParser(filepath)
-            rwx_object = parser()
-        except Exception as exc:
-            print_exc(exc)
-            self.report({'ERROR'}, "Could not parse input file (either not a proper .rwx or a bug on my part).")
-            return {'CANCELLED'}
+            if len(rwx_object.clumps) == 0:
+                self.report({'ERROR'}, "No clump registered after parsing the input file (likely not a proper .rwx).")
+                return {'CANCELLED'}
 
-        if len(rwx_object.clumps) == 0:
-            self.report({'ERROR'}, "No clump registered after parsing the input file (likely not a proper .rwx).")
-            return {'CANCELLED'}
-
-        verts = add_vertices_recursive(rwx_object.clumps[0])
-        edges = []
-        faces, offset = add_faces_recursive(rwx_object.clumps[0])
-        faces_state = add_attr_recursive(rwx_object.clumps[0], "faces_state")
-        faces_uv = add_attr_recursive(rwx_object.clumps[0], "faces_uv")
-
-        mesh = bpy.data.meshes.new('Mesh')
-        ob = bpy.data.objects.new('Object', mesh)
+            verts = add_vertices_recursive(rwx_object.clumps[0])
+            edges = []
+            faces, offset = add_faces_recursive(rwx_object.clumps[0])
+            faces_state = add_attr_recursive(rwx_object.clumps[0], "faces_state")
+            faces_uv = add_attr_recursive(rwx_object.clumps[0], "faces_uv")
+            
+            mesh = bpy.data.meshes.new('Mesh')
+            ob = bpy.data.objects.new('Object', mesh)
         
-        make_materials_recursive(ob, rwx_object.clumps[0], texturepath)
+            make_materials_recursive(ob, rwx_object.clumps[0], texturepath)
 
-        # Create mesh from given verts, edges, faces. Either edges or
-        # faces should be [], or you ask for problems
-        mesh.from_pydata(verts, edges, faces)
-        bm = bmesh.new()
-        bm.from_mesh(mesh)
-        uv_layer = bm.loops.layers.uv.verify()
-        bm.faces.layers.tex.verify()  # currently blender needs both layers.
+            # Create mesh from given verts, edges, faces. Either edges or
+            # faces should be [], or you ask for problems
+            mesh.from_pydata(verts, edges, faces)
+            bm = bmesh.new()
+            bm.from_mesh(mesh)
 
-        # adjust UVs
-        for i, f in enumerate(bm.faces):
-            if faces_state[i].texture:
-                f.material_index = ob.data.materials.keys().index(faces_state[i].texture)
+            uv_layer = bm.loops.layers.uv.verify()
+            bm.faces.layers.tex.verify()  # currently blender needs both layers.
+
+            if uv_layer is None:
+                uv_layer = bm.loops.layers.uv.new()
+            
+            # adjust UVs
+            for i, f in enumerate(bm.faces):
+                f.material_index = ob.data.materials.keys().index(faces_state[i].mat_signature)
                 for j, l in enumerate(f.loops):
-                    l[uv_layer].uv = faces_uv[i][j]
+                    uv = faces_uv[i][j]
+                    if uv[0] is not None and uv[1] is not None:
+                        l[uv_layer].uv = uv
+                    
+            bm.to_mesh(mesh)
+            bm.free()
 
+            mesh.use_auto_smooth = True
+            mesh.auto_smooth_angle = 3.14/3.0 
+            mesh.calc_normals()
 
-        bm.to_mesh(mesh)
-        bm.free()
+            ob.location = (0,0,0)
+            ob.scale = (10,10,10)
+            ob.rotation_euler = (radians(90), 0, 0)
+            ob.show_name = True 
+            # Link object to scene
+            bpy.context.scene.objects.link(ob)
 
-        mesh.use_auto_smooth = True
-        mesh.auto_smooth_angle = 3.14/3.0 
-        mesh.calc_normals()
-
-        ob.location = (0,0,0)
-        ob.scale = (10,10,10)
-        ob.rotation_euler = (radians(90), 0, 0)
-        ob.show_name = True 
-        # Link object to scene
-        bpy.context.scene.objects.link(ob)
-
-        # Update mesh with new data
-        mesh.update(calc_edges=True)
+            # Update mesh with new data
+            mesh.update(calc_edges=True)
         
-        return {'FINISHED'}
+            return {'FINISHED'}
 
 def import_menu_func_rwx(self, context):
     op = self.layout.operator(Rwx2BlenderOperator.bl_idname, text="Active Worlds RenderWare file (.rwx)")
@@ -548,5 +610,6 @@ def unregister():
     bpy.types.INFO_MT_file_import.remove(import_menu_func_rwx)
 
 if __name__ == "__main__":
-    register()
+    
+    if in_blender: register()
 
